e9d46a1c5572fbb3be66b246c133a1e266fdf736
vinips1993 <rm550907@fiap.com.br>
2023-04-27 22:20:10 -0300

    inclusão_variavel_let

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("Alô Mundo!");
+var nome = "Luizinho";
+
+if(nome != ""){
+    let nome = "Nicolly";
+}
+
+console.log(nome);
+//Quando declaramos uma variável com var
+// o Javascript aplica um processo nestas que chamamos de HOISTING.
+//O HOISTING ou elevação, coloca todas as variáveis do tipo var no topo do programa.
+//Isso faz com que se existirem variáveis com mesmo nome, elas se sobrepõem.
+
